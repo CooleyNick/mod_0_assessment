3313eaec3b68f6177e3cde288bef41cbea75ec95
Nick Cooley <cooley8706@gmail.com>
2021-09-03 10:20:39 -0600

    Model dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+"name" => 'Tyrannosaurus Rex',
+"location" => 'Jurassic Park',
+"height_in_feet" => 18,
+"clocked_speed_mph" => 32,
+"is_angry" => true,
+"likes_captivity" => false,
+"enjoys_eating" => ['goats', 'lawyers', 'Velociraptor'],
+"hobbies" => ['golf', 'mountain climbing', 'reading'],
+}
