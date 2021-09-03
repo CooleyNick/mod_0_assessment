40268aa61648ee4959ce6476980f803e1fe3484a
Nick Cooley <cooley8706@gmail.com>
2021-09-03 10:05:07 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,11 @@
+movie = "Dumb and Dumber";
+is_entertaining = true;
+
+movie = "Talladega Nights";
+num_4 = 4;
+num_6 = 6;
+product = num_4 * num_6;
+
+favorite_foods = ['watermelon', 'steak', 'sushi'];
+arr.shift(1);
+puts "Talladega Nights".length;
