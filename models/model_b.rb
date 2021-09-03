3da669455aa331a078fb875ce59ff43eae793f22
Nick Cooley <cooley8706@gmail.com>
2021-09-03 10:26:42 -0600

    Model developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+"name" => 'Nick Cooley',
+"location" => 'Denver, CO',
+"age" => 34,
+"number_of_4Runners" => 1,
+"wants_to_mountain_bike" => true,
+"wants_to_paint_house" => false,
+"house_projects" => ['paint', 'sprinkers', 'mow'],
+"favorite_towns" => ['Breckenridge', 'Steamboat', 'Denver'],
+}
